143c4c7e044c87a3c006d5a5b5e1eff35788b26d
Aaryanpatidar <aaryanpatidar1211@gmail.com>
2026-05-11 20:36:15 +0530

    url fixing

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -5,7 +5,7 @@
 
 import axios from 'axios';
 
-const BASE_URL = `${import.meta.env.VITE_API_URL}/api` || '/api';
+const BASE_URL = `${import.meta.env.VITE_API_URL}api` || '/api';
 
 const api = axios.create({
   baseURL: BASE_URL,
